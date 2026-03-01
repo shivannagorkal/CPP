53cef7fcbfe40101589a2a149964f1ccd5271c6b
shivannagorkal <shivannagorkal@gmail.com>
2026-03-01 20:12:33 +0530

    Fix Day-01 folder

---
+++ Day-02/p01.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int a, b;
+    cout << "Enter two numbers: ";  
+    cin >> a >> b; // Read two integers from input and store them in variables a and b.
+    int sum = a + b; // Calculate the sum of the two numbers and store it in variable sum.
+    cout << "The sum of " << a << " and " << b << " is: " << sum << endl; // Print the result in the format "The sum of [a] and [b] is: [sum]".
+    
+
+    return 0;
+}
