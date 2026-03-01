57f0a68c6287a3b3a3839d804ead45c9ecb8b56a
shivannagorkal <shivannagorkal@gmail.com>
2026-03-01 19:56:02 +0530

    first commit

---
+++ Day-02/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+
+    // Calculate the sum of the two numbers
+    int sum = a + b;
+
+    // Output the result
+    cout << "The sum of " << a << " and " << b << " is: " << sum << endl;
+
+    return 0;
+}
