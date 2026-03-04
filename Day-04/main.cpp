e8273e4d62a895f89798d8daaa1c4d5d8471f14f
shivannagorkal <shivannagorkal@gmail.com>
2026-03-04 20:55:55 +0530

    add day-04

---
+++ Day-04/main.cpp
@@ -0,0 +1,23 @@
+// FOR LOOP (Most Used in CP)
+// syntax:
+// for(initialization; condition; update)
+// {
+//     // code
+// }
+
+// Example: Print first 10 natural numbers
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    // Example: Print first 10 natural numbers
+    
+    for(int i = 1; i <= 10; i++)
+    {
+        cout << i << " ";
+    }
+
+    return 0;
+}
