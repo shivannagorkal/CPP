e5e410db20b171523a7d3c80149d2b56a127b50d
shivannagorkal <shivannagorkal@gmail.com>
2026-03-05 21:03:38 +0530

    add p-01 file

---
+++ Day-04/p-01.cpp
@@ -0,0 +1,16 @@
+// Program to find the sum of all elements in an array
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int sum = 0;
+    
+    for (int i = 0; i < n; i++) {
+        sum += arr[i];
+    }
+    
+    cout << "Sum of array elements: " << sum << endl;
+    return 0;
+}
